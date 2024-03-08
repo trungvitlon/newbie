a93ce9dd20884848a4fe4ca74564131d19b20873
trungvitlon <dovietlong2105@gmail.com>
2024-03-08 12:08:06 +0700

    Add new project ...

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+        int a, b;
+        cout << "Enter number:" << '\n';
+        cin >> a >> b;
+        cout << "sum is:" << a + b << '\n';
+        return 0;
+}
